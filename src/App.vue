0c0af69b0ce01b8bc4dd3621c4c574dfd67b7067
l-pizarro <leandro.pizarro@usach.cl>
2019-04-29 10:58:51 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <Actors :actors="actorsFromDB"/>
+        </div>
+        <div class="col-6">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Actors from './components/Actors.vue'
+import axios from 'axios'
+
+export default {
+  name: 'app',
+
+  components: {
+    Actors,
+    HelloWorld
+  },
+
+  data() {
+    return {
+      actorsFromDB: []
+    }
+  },
+
+  created() {
+     axios.get('/http://localhost:8081/actors')
+        .then( (response) => {
+            this.actorsFromDB = response.data;
+            }
+            );
+  },
+
+  mounted() {
+    axios.get('/http://localhost:8081/actors')
+        .then( (response) => {
+            this.actorsFromDB = response.data;
+            }
+            );
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Actors.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="actors">
+    {{ actors }}
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Actors',
+
+    props: [
+        'actors'
+    ],
+
+    data() {
+        return {
+            myActors: []
+        }
+    },
+
+    munted() {
+        console.log("Component mounted");
+    },
+
+    creted() {
+        console.log("Component created")
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
